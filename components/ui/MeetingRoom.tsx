9c8f28c68866f12bc0a16c0b9d395d35f9f92d73
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-05-08 14:35:10 -0400

    Meeting room

--- components/ui/MeetingRoom.tsx
+++ components/ui/MeetingRoom.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { PaginatedGridLayout, SpeakerLayout } from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
+
+type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
-  return (
-    <div>MeetingRoom</div>
-  )
-}
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
 
-export default MeetingRoom
+  const CallLayout = () => {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout />;
+      case "speaker-right":
+        return <SpeakerLayout participantsBarPosition='left' />;
+      default:
+        return <SpeakerLayout participantsBarPosition='right' />;
+
+    }
+  };
+
+  return (
+    <section
+      className="relative h-screen w-full 
+    overflow-hidden pt-4 text-white"
+    >
+      <div
+        className="relative flex size-full 
+      items-center justify-center"
+      >
+        <div
+          className="flex size-full m-w-[1000px] 
+        items-center"
+        >
+          <CallLayout />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MeetingRoom;
