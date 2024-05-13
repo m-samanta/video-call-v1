830a4cb74e95b7e8f37d89b871e193f02bc4138d
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-05-13 14:19:10 -0400

    Schedule meeting 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
-import "@stream-io/video-react-sdk/dist/css/styles.css"
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import "react-datepicker/dist/react-datepicker.css";
 
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +14,8 @@ export const metadata: Metadata = {
   title: "YOOM",
   description: "Video calling app",
   icons: {
-    icon: '/icons/logo.svg'
-  }
+    icon: "/icons/logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -25,19 +26,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ClerkProvider
-      appearance={{
-        layout: {
-          logoImageUrl: '/icons/yoom-logo.svg',
-          socialButtonsVariant: 'iconButton'
-        },
-        variables: {
-          colorText: '#fff',
-          colorPrimary: '#0E78F9',
-          colorBackground: '#1c1f2e',
-          colorInputBackground: '#252a41',
-          colorInputText: '#fff'
-        }
-      }}
+        appearance={{
+          layout: {
+            logoImageUrl: "/icons/yoom-logo.svg",
+            socialButtonsVariant: "iconButton",
+          },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1c1f2e",
+            colorInputBackground: "#252a41",
+            colorInputText: "#fff",
+          },
+        }}
       >
         <body className={`${inter.className} bg-dark-2`}>
           {children}
