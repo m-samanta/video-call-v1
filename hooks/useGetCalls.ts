1d65b3de957372d535dffb98a3548941c5f7d82d
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-05-14 12:55:00 -0400

    Upcoming meetings page

---
+++ hooks/useGetCalls.ts
@@ -0,0 +1,26 @@
+import { useUser } from "@clerk/nextjs"
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk"
+import { useEffect, useState } from "react"
+
+export const useGetCalls = () => {
+    const [calls, setCalls] = useState<Call[]>([])
+    const [isLoading, setIsLoading] = useState(false)
+    const client = useStreamVideoClient()
+    const { user } = useUser()
+
+    useEffect(() => {
+        const loadCalls = async () => {
+            if(!client || !user?.id) return
+            setIsLoading(true)
+
+            try {
+                const { calls } = await client.queryCalls()
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+        loadCalls()
+    }, [client, user?.id])
+}
