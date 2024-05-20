61d707e3aec7c46bf82fff97f867f466f906ebaf
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-05-20 19:34:14 -0400

    Personal meeting room 2

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -10,21 +10,28 @@ const Table = ({
   title: string;
   description: string;
 }) => (
-  <div>
-    <h1>{title}:</h1>
-    <h1>{description}</h1>
+  <div className="flex flex-col items-start gap-2 xl:flex-row">
+    <h1 className="text-base font-medium text-sky-1 lg:text-xl xl:min-w-32">
+      {title}:
+    </h1>
+    <h1 className="truncate text-sm font-bold max-sm:max-w-[320px] lg:text-xl">
+      {description}
+    </h1>
   </div>
 );
 
 const PersonalRoom = () => {
   const { user } = useUser();
+  const meetingId = user?.id;
 
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-bold">Personal Room</h1>
 
       <div className="flex w-full flex-col gap-8 xl:max-w-[900px]">
-        <Table title="Topic" description={`${user?.username}'s Meeting Room`} />
+        <Table title="Topic" description={`${user?.username}'s meeting room`} />
+        <Table title="Meeting ID" description={meetingId!} />
+        <Table title="Topic" description={`${user?.username}'s meeting room`} />
       </div>
     </section>
   );
